1e8969bc64d7f80f023058b0abceeb78a264d2db
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-04-06 10:16:29 +0700

    master

---
+++ src/components/travels/HeaderView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import IconAirplane from '../icons/travels/IconAirplane.vue'
+</script>
+
+<template>
+  <header class="header flex between">
+    <div class="logo">
+      <span class="subtitle">Velsnesia</span>
+      <span class="icon"><IconAirplane /></span>
+    </div>
+    <ul class="nav gap flex">
+      <li class="list active"><a href="#home" class="link">Home</a></li>
+      <li class="list"><a href="#destinations" class="link">Destinations</a></li>
+      <li class="list"><a href="#about" class="link">How it works</a></li>
+      <li class="list"><a href="/login" class="link btn primary">Login</a></li>
+    </ul>
+  </header>
+</template>
---
+++ src/views/travels/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Header from '@/components/travels/HeaderView.vue'
+</script>
+
+<template>
+  <section class="container">
+    <Header />
+    <div class="homepage flex center">
+      <div class="img homepage_img">
+        <img src="@/components/images/homepage.jpg" alt="Homepage" class="photo" />
+      </div>
+      <div class="content">
+        <h2 class="title">
+          Explore the Beauty of Nature <br />
+          Discover the New You
+        </h2>
+        <form action="" class="form">
+          <div class="input-form full-content flex gap">
+            <div class="field">
+              <label for="">Location</label>
+              <input type="text" placeholder="Yogyakarta" />
+            </div>
+            <div class="field">
+              <label for="">Check In</label>
+              <input type="date" />
+            </div>
+            <div class="field">
+              <label for="">Check Out</label>
+              <input type="date" />
+            </div>
+            <div class="field">
+              <label for="">Guests</label>
+              <select name="" id="">
+                <option value="-">Option</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+</template>
