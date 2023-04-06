8ea4c986ba969f2718830a7c97b0d438c51280d1
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-04-06 08:56:19 +0700

    master

---
+++ README.md
@@ -0,0 +1,7 @@
+##### From 6 April 2023
+
+## Velsnesia | Travel Web
+
+### First make project with Vue 3 and SASS
+
+---
---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button class="btn primary">Hello</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
